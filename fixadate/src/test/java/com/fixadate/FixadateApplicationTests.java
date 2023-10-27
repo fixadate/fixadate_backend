98bf5988d36c7e83f96ac3c3642c8498c9107922
sungjun4403 <geulligu89@naver.com>
2023-10-28 00:10:48 +0900

    spring initilizar

---
+++ fixadate/src/test/java/com/fixadate/FixadateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixadate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixadateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
