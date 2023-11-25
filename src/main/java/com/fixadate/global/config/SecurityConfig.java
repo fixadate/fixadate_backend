ccf47d0184d67850a69b523fcfaface4c782e4fd
hong <kevin0928@naver.com>
2023-11-25 16:54:28 +0900

    :sparkles: : oauth2Login을 위해 설정 추가
    - oauth2Login, successHandler, FailureHandler 설정 추가함

--- src/main/java/com/fixadate/global/config/SecurityConfig.java
+++ src/main/java/com/fixadate/global/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.fixadate.global.config;
 
 import com.fixadate.domain.member.repository.MemberRepository;
+import com.fixadate.global.jwt.service.JwtProvider;
+import com.fixadate.global.oauth.handler.OAuth2MemberSuccessHandler;
+import com.fixadate.global.oauth.service.CustomOAuth2UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
@@ -14,8 +18,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfig extends SecurityConfigurerAdapter {
+    private final JwtProvider jwtProvider;
+    private final CustomOAuth2UserService customOAuth2UserService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(); // deprecated for spring security 7.0 (available for now)
@@ -25,6 +31,12 @@ public class SecurityConfig extends SecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.formLogin().disable();
         http.httpBasic().disable();
+
+        http
+                .oauth2Login()
+                .successHandler(new OAuth2MemberSuccessHandler(jwtProvider))
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
 
         return http.build();
     }
