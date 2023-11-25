454bda902ff701f0eaf10b86878551539fd9bfdb
hong <kevin0928@naver.com>
2023-11-25 16:50:23 +0900

    :bug: : 성별, 생년월일, 프로필 이미지 받아올 수 있게 수정
    - scope도 동일하게 수정함

--- src/main/java/com/fixadate/global/oauth/entity/OAuthAttributes.java
+++ src/main/java/com/fixadate/global/oauth/entity/OAuthAttributes.java
@@ -8,26 +8,35 @@ import java.util.Map;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @ToString
+@Slf4j
 public class OAuthAttributes {
     private Map<String, Object> attributes;     // OAuth2 반환하는 유저 정보
     private String oauthId;
     private String nameAttributesKey;
-    private String name;
     private String email;
+    private String gender;
+    private int age;
+    private String birthday;
     private String oAuthProvider;
+    private String profileImage;
 
     @Builder
     public OAuthAttributes(String oauthId, Map<String, Object> attributes, String nameAttributesKey,
-                           String name, String email, String oAuthProvider) {
+                           String email, String gender, int age, String birthday, String oAuthProvider,
+                           String profileImage) {
+        this.gender = gender;
+        this.age = age;
+        this.birthday = birthday;
         this.oauthId = oauthId;
         this.attributes = attributes;
         this.nameAttributesKey = nameAttributesKey;
-        this.name = name;
         this.email = email;
         this.oAuthProvider = oAuthProvider;
+        this.profileImage = profileImage;
     }
 
     public static OAuthAttributes of(String socialName, Map<String, Object> attributes) {
@@ -35,8 +44,6 @@ public class OAuthAttributes {
             return ofKakao("id", attributes);
         } else if ("google".equals(socialName)) {
             return ofGoogle("sub", attributes);
-        } else if ("naver".equals(socialName)) {
-            return ofNaver("id", attributes);
         }
         return null;
     }
@@ -44,7 +51,6 @@ public class OAuthAttributes {
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .oauthId(String.valueOf(attributes.get(GOOGLE_OAUTH_ID)))
-                .name(String.valueOf(attributes.get("name")))
                 .email(String.valueOf(attributes.get(EMAIL)))
                 .oAuthProvider(OAuthProvider.Google.getProvider())
                 .attributes(attributes)
@@ -54,30 +60,20 @@ public class OAuthAttributes {
 
     private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
         String oauthId = String.valueOf(attributes.get(KAKAO_NAVER_OAUTH_ID));
+        log.info(attributes.toString());
 
         Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+//        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
 
         return OAuthAttributes.builder()
                 .oauthId(oauthId)
-                .name(String.valueOf(kakaoProfile.get(NICK_NAME)))
+                .gender(String.valueOf(kakaoAccount.get("gender")))
+                .birthday(String.valueOf(kakaoAccount.get("birthday")))
+                .profileImage(String.valueOf(kakaoAccount.get("profile_image")))
                 .email(String.valueOf(kakaoAccount.get(EMAIL)))
                 .oAuthProvider(OAuthProvider.Kakao.getProvider())
                 .nameAttributesKey(userNameAttributeName)
                 .attributes(attributes)
-                .build();
-    }
-
-    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
-        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
-
-        return OAuthAttributes.builder()
-                .oauthId(String.valueOf(response.get(KAKAO_NAVER_OAUTH_ID)))
-                .name(String.valueOf(response.get(NICK_NAME)))
-                .email(String.valueOf(response.get(EMAIL)))
-                .oAuthProvider(OAuthProvider.Naver.getProvider())
-                .attributes(response)
-                .nameAttributesKey(userNameAttributeName)
                 .build();
     }
 
@@ -86,10 +82,11 @@ public class OAuthAttributes {
         todo : oauth로그인 할 때 nickname, email, OAuthId를 얻어오는데 profileImg도 얻어오도록 구현하기.
          */
         return Member.builder()
-                .name(name)
+                .birth(Integer.parseInt(birthday))
+                .gender(gender)
+                .oauthPlatform(oAuthProvider)
                 .oauthId(oauthId)
                 .oauthPlatform(oAuthProvider)
-
                 .build();
     }
 }
