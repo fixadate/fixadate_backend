31c705c458f864ff569d64c6ad5e3ffed046fd03
hong <kevin0928@naver.com>
2023-12-17 17:00:58 +0900

    :sparkles: : 사용자의 nickname 랜덤 생성 구현

--- src/main/java/com/fixadate/domain/member/controller/MemberController.java
+++ src/main/java/com/fixadate/domain/member/controller/MemberController.java
@@ -4,8 +4,12 @@ import com.fixadate.domain.member.dto.request.AdateColorNameRequestDto;
 import com.fixadate.domain.member.entity.Member;
 import com.fixadate.domain.member.service.MemberService;
 import com.fixadate.global.jwt.MemberPrincipal;
+import java.util.List;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,8 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
     private final MemberService memberService;
+
+    @Value("${randNick.adjs}")
+    private String randomAdjs;
+    @Value("${randNick.nouns}")
+    private String randomNouns;
 
     @PostMapping("/member/regist/color")
     public ResponseEntity<String> registAdateColorAndName(
@@ -30,4 +40,16 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("color 등록 완료");
     }
+
+    @PostMapping("/member/random/nickname")
+    public ResponseEntity<?> getRandomNickname() {
+        List<String> adjs = List.of(randomAdjs.split(","));
+        List<String> nouns = List.of(randomNouns.split(","));
+
+        String adj = memberService.getRandomNickname(adjs);
+        String noun = memberService.getRandomNickname(nouns);
+        String randomNickname = adj + " " + noun;
+        return ResponseEntity.ok(randomNickname);
+    }
+
 }
--- src/main/java/com/fixadate/domain/member/service/MemberService.java
+++ src/main/java/com/fixadate/domain/member/service/MemberService.java
@@ -7,8 +7,10 @@ import com.fixadate.domain.member.exception.UnknownMemberException;
 import com.fixadate.domain.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +21,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
+    private final Random random = new Random(System.currentTimeMillis());
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -32,6 +36,7 @@ public class MemberService implements UserDetailsService {
     public Optional<Member> findMemberByOAuthId(String oAuthId) {
         return memberRepository.findMemberByOauthId(oAuthId);
     }
+
     @Transactional
     public void saveAdateColorAndName(AdateColorNameRequestDto adateColorNameRequestDto, Member member) {
         Map<String, String> adateColorTypes = member.getAdateColorTypes();
@@ -51,7 +56,13 @@ public class MemberService implements UserDetailsService {
     public Member getMemberWithAdateColorTypes(Long memberId) {
         Member member = memberRepository.findById(memberId).orElseThrow(
                 () -> new UnknownMemberException());
-        member.getAdateColorTypes().forEach((key, value) -> {});
+        member.getAdateColorTypes().forEach((key, value) -> {
+        });
         return member;
+    }
+
+    public String getRandomNickname(List<String> strings) {
+
+        return strings.get(random.nextInt(strings.size())).trim();
     }
 }
