5355fed39913afb3968cf967f8b31e75fb7de3db
hong <kevin0928@naver.com>
2023-12-29 15:02:40 +0900

    :ambulance: : meaningless modify to deploy

--- src/main/java/com/fixadate/domain/adate/repository/AdateRepository.java
+++ src/main/java/com/fixadate/domain/adate/repository/AdateRepository.java
@@ -3,5 +3,5 @@ package com.fixadate.domain.adate.repository;
 import com.fixadate.domain.adate.entity.Adate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdateRepository extends JpaRepository<Adate, Long> {
+public interface AdateRepository extends JpaRepository<Adate, Long>   {
 }
