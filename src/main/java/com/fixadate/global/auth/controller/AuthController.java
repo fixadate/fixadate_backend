982290a26aa9e1864145ee1f277f14d77a6696e8
yongjunhong <kevin0928@naver.com>
2024-01-12 13:48:46 +0900

    :recycle: : url restful하게 변경

--- src/main/java/com/fixadate/global/auth/controller/AuthController.java
+++ src/main/java/com/fixadate/global/auth/controller/AuthController.java
@@ -43,7 +43,7 @@ public class AuthController {
         return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
     }
 
-    @PostMapping("/auth/additional/member")
+    @PostMapping("/auth/member/additional")
     public ResponseEntity<Void> AdditionalRegistMember(
             @RequestBody @Validated MemberRegistRequestDto memberRegistRequestDto) {
         authService.registMember(memberRegistRequestDto);
