203265ae52ac803f4348f5050c59ad861133f1b3
yongjunhong <kevin0928@naver.com>
2024-02-04 16:49:54 +0900

    :zap: : auth에서 email 삭제

--- src/main/java/com/fixadate/global/oauth/ConstantValue.java
+++ src/main/java/com/fixadate/global/oauth/ConstantValue.java
@@ -5,7 +5,6 @@ public class ConstantValue {
     public static final String ACCESS_TOKEN = "accessToken";
     public static final String REFRESH_TOKEN = "refreshToken";
     public static final String NICK_NAME = "nickname";
-    public static final String EMAIL = "email";
     public static final String GOOGLE_OAUTH_ID = "sub";
     public static final String KAKAO_NAVER_OAUTH_ID = "id";
 }
