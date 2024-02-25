677e047ff8d2bb12ea84ab7c89cf5b6e252058f5
yongjunhong <kevin0928@naver.com>
2024-02-25 15:46:40 +0900

    test

--- src/main/java/com/fixadate/domain/adate/controller/AdateController.java
+++ src/main/java/com/fixadate/domain/adate/controller/AdateController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-//
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/calendar")
