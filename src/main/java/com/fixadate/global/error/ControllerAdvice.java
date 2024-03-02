63759f5faaf6e0a466db4c659d8554368bbe8fe1
yongjunhong <kevin0928@naver.com>
2024-03-02 13:57:50 +0900

    FIX-5 :recycle: : JWT Provider 리팩토링
    - ExceptionHandlerFilter에서 에러를 catch하므로 JwtProvider에서 throw 할 필요가 없음

--- src/main/java/com/fixadate/global/error/ControllerAdvice.java
+++ src/main/java/com/fixadate/global/error/ControllerAdvice.java
@@ -7,22 +7,16 @@ import com.fixadate.domain.invitation.exception.InvitationNotFountException;
 import com.fixadate.domain.member.exception.MemberNotFoundException;
 import com.fixadate.global.auth.exception.MemberSigninException;
 import com.fixadate.global.auth.exception.UnknownOAuthPlatformException;
-import com.fixadate.global.jwt.exception.TokenException;
-import com.fixadate.global.jwt.exception.TokenExpiredException;
-import com.fixadate.global.jwt.exception.TokenUnsupportedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 public class ControllerAdvice {
     @ExceptionHandler({
             MemberNotFoundException.class,
             UnknownOAuthPlatformException.class,
-            SQLIntegrityConstraintViolationException.class,
             InvitationNotFountException.class,
             MemberSigninException.class,
             ColorTypeNotFoundException.class
--- src/main/java/com/fixadate/global/jwt/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/fixadate/global/jwt/filter/ExceptionHandlerFilter.java
@@ -1,15 +1,16 @@
 package com.fixadate.global.jwt.filter;
 
-import com.fixadate.global.error.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fixadate.global.jwt.exception.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SecurityException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -19,11 +20,11 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
-        } catch (TokenExpiredException exception) {
+        } catch (ExpiredJwtException exception) {
             setErrorResponse(response, JwtException.JWT_EXPIRED_EXCEPTION);
-        } catch (TokenUnsupportedException exception) {
+        } catch (UnsupportedJwtException exception) {
             setErrorResponse(response, JwtException.JWT_UNSUPPORTED_EXCEPTION);
-        } catch (TokenException exception) {
+        } catch (SecurityException | MalformedJwtException | IllegalArgumentException e) {
             setErrorResponse(response, JwtException.JWT_EXCEPTION);
         }
     }
