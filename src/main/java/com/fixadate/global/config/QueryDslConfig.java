c093b095445e772b840898c9f0277049544ad809
yongjunhong <kevin0928@naver.com>
2024-03-02 15:16:55 +0900

    FIX-5 :recycle: : 생성자 주입으로 변경

--- src/main/java/com/fixadate/global/config/QueryDslConfig.java
+++ src/main/java/com/fixadate/global/config/QueryDslConfig.java
@@ -2,17 +2,17 @@ package com.fixadate.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class QueryDslConfig {
+    private final EntityManager em;
 
-    @Autowired
-    private EntityManager em;
     @Bean
-    public JPAQueryFactory jpaQueryFactory(){
+    public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(em);
     }
 }
