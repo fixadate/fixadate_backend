727bd326bafae8e676f13fbef6127c477c8dc0e7
yongjunhong <kevin0928@naver.com>
2024-04-17 21:40:24 +0900

    FIX-3 :white_check_mark: : 테스트의 격리성을 높이기 위해, test 시작전에 table을 초기화 구현

---
+++ src/test/resources/sql/init/dropTable.sql
@@ -0,0 +1,7 @@
+SET FOREIGN_KEY_CHECKS = 0; -- 외래 키 체크 해제
+DROP TABLE IF EXISTS color_type;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS adate;
+DROP TABLE IF EXISTS google_credentials;
+
+SET FOREIGN_KEY_CHECKS = 1; -- 외래 키 체크 다시 활성화
