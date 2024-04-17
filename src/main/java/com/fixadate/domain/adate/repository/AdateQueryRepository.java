eee7e6a229e3da4f5c86e2e021692e88e52e2063
yongjunhong <kevin0928@naver.com>
2024-04-17 19:46:26 +0900

    FIX-3 :bug: : or에서 and 연산으로 변경

--- src/main/java/com/fixadate/domain/adate/repository/AdateQueryRepository.java
+++ src/main/java/com/fixadate/domain/adate/repository/AdateQueryRepository.java
@@ -2,18 +2,15 @@ package com.fixadate.domain.adate.repository;
 
 import com.fixadate.domain.adate.entity.Adate;
 import com.fixadate.domain.member.entity.Member;
-import com.fixadate.domain.member.entity.QMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-import static com.fixadate.domain.adate.entity.QAdate.*;
-import static com.fixadate.domain.member.entity.QMember.*;
+import static com.fixadate.domain.adate.entity.QAdate.adate;
+import static com.fixadate.domain.member.entity.QMember.member;
 
 @Repository
 @RequiredArgsConstructor
@@ -24,8 +21,8 @@ public class AdateQueryRepository {
         return jpaQueryFactory
                 .selectFrom(adate)
                 .where(adate.member.eq(member))
-                .where(adate.endsWhen.goe(startDateTime).
-                        or(adate.startsWhen.loe(endDateTime)))
+                .where(adate.endsWhen.goe(startDateTime)
+                        .and(adate.startsWhen.loe(endDateTime)))
                 .orderBy(adate.startsWhen.asc())
                 .fetch();
     }
