485a0750ef8d6e2c01d2ecf4b05d94931c936f6f
yongjunhong <kevin0928@naver.com>
2024-05-11 22:56:28 +0900

    FIX-5 :recycle: : config 코딩 컨벤션 적용 (naver 코딩 컨벤션)

--- src/main/java/com/fixadate/global/config/SwaggerConfig.java
+++ src/main/java/com/fixadate/global/config/SwaggerConfig.java
@@ -1,30 +1,31 @@
 package com.fixadate.global.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import static io.swagger.v3.oas.models.security.SecurityScheme.*;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Fixadate API 명세서")
-                        .description("Fixadate의 API 문서입니다.")
-                        .version("v1"))
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key",
-                                new io.swagger.v3.oas.models.security.SecurityScheme()
-                                        .type(Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Fixadate API 명세서")
+				.description("Fixadate의 API 문서입니다.")
+				.version("v1"))
+			.components(new Components()
+				.addSecuritySchemes("bearer-key",
+					new io.swagger.v3.oas.models.security.SecurityScheme()
+						.type(Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")));
 
-    }
+	}
 
 }
