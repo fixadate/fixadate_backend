3ec25a98dfa34f8bbb9f5e9340d4baf2b301c9e2
yongjunhong <kevin0928@naver.com>
2024-05-11 22:53:49 +0900

    FIX-5 :recycle: : googleCalendar 코딩 컨벤션 적용 (naver 코딩 컨벤션)

--- src/main/java/com/fixadate/domain/googleCalendar/controller/impl/GoogleCalendarControllerImpl.java
+++ src/main/java/com/fixadate/domain/googleCalendar/controller/impl/GoogleCalendarControllerImpl.java
@@ -1,15 +1,9 @@
 package com.fixadate.domain.googleCalendar.controller.impl;
 
-import com.fixadate.domain.googleCalendar.controller.GoogleCalendarController;
-import com.fixadate.domain.googleCalendar.dto.response.GoogleCalendarEventResponse;
-import com.fixadate.domain.googleCalendar.entity.constant.WebhookHeaders;
-import com.fixadate.domain.googleCalendar.service.GoogleService;
-import com.fixadate.global.annotation.RestControllerWithMapping;
-import com.google.api.client.auth.oauth2.TokenResponse;
-import com.google.api.services.calendar.model.Channel;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import static com.fixadate.global.util.constant.ConstantValue.*;
+
+import java.util.List;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,53 +12,60 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.fixadate.domain.googleCalendar.controller.GoogleCalendarController;
+import com.fixadate.domain.googleCalendar.dto.response.GoogleCalendarEventResponse;
+import com.fixadate.domain.googleCalendar.entity.constant.WebhookHeaders;
+import com.fixadate.domain.googleCalendar.service.GoogleService;
+import com.fixadate.global.annotation.RestControllerWithMapping;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.services.calendar.model.Channel;
 
-import static com.fixadate.global.util.constant.ConstantValue.*;
-
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 
 @RestControllerWithMapping("/google")
 @RequiredArgsConstructor
 public class GoogleCalendarControllerImpl implements GoogleCalendarController {
-    private final GoogleService googleService;
+	private final GoogleService googleService;
 
-    @Override
-    @GetMapping("/watch")
-    public ResponseEntity<Channel> watchCalendar(@RequestParam String userId,
-                                                 @RequestParam String memberId,
-                                                 HttpServletRequest request) {
-        TokenResponse tokenResponse = createTokenResponse(request.getCookies());
-        String accessToken = AUTHORIZATION_BEARER.getValue() + tokenResponse.getAccessToken();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(AUTHORIZATION.getValue(), accessToken);
+	@Override
+	@GetMapping("/watch")
+	public ResponseEntity<Channel> watchCalendar(@RequestParam String userId,
+		@RequestParam String memberId, HttpServletRequest request) {
+		TokenResponse tokenResponse = createTokenResponse(request.getCookies());
+		String accessToken = AUTHORIZATION_BEARER.getValue() + tokenResponse.getAccessToken();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(AUTHORIZATION.getValue(), accessToken);
 
-        Channel channel = googleService.executeWatchRequest(userId);
-        googleService.registGoogleCredentials(channel, tokenResponse, userId, memberId);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .headers(headers).build();
-    }
+		Channel channel = googleService.executeWatchRequest(userId);
+		googleService.registGoogleCredentials(channel, tokenResponse, userId, memberId);
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.headers(headers).build();
+	}
 
-    private TokenResponse createTokenResponse(Cookie[] cookies) {
-        TokenResponse tokenResponse = new TokenResponse();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals(ACCESS_TOKEN.getValue())) {
-                tokenResponse.setAccessToken(cookie.getValue());
-            } else if (cookie.getName().equals(REFRESH_TOKEN.getValue())) {
-                tokenResponse.setRefreshToken(cookie.getValue());
-            }
-        }
-        return tokenResponse;
-    }
+	private TokenResponse createTokenResponse(Cookie[] cookies) {
+		TokenResponse tokenResponse = new TokenResponse();
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals(ACCESS_TOKEN.getValue())) {
+				tokenResponse.setAccessToken(cookie.getValue());
+			} else if (cookie.getName().equals(REFRESH_TOKEN.getValue())) {
+				tokenResponse.setRefreshToken(cookie.getValue());
+			}
+		}
+		return tokenResponse;
+	}
 
-    @Override
-    @PostMapping("/notifications")
-    public ResponseEntity<List<GoogleCalendarEventResponse>> printNotification(@RequestHeader(WebhookHeaders.RESOURCE_ID) String resourceId,
-                                                                               @RequestHeader(WebhookHeaders.RESOURCE_URI) String resourceUri,
-                                                                               @RequestHeader(WebhookHeaders.CHANNEL_ID) String channelId,
-                                                                               @RequestHeader(WebhookHeaders.CHANNEL_EXPIRATION) String channelExpiration,
-                                                                               @RequestHeader(WebhookHeaders.RESOURCE_STATE) String resourceState,
-                                                                               @RequestHeader(WebhookHeaders.MESSAGE_NUMBER) String messageNumber) {
-        googleService.listEvents(channelId);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+	@Override
+	@PostMapping("/notifications")
+	public ResponseEntity<List<GoogleCalendarEventResponse>> printNotification(
+		@RequestHeader(WebhookHeaders.RESOURCE_ID) String resourceId,
+		@RequestHeader(WebhookHeaders.RESOURCE_URI) String resourceUri,
+		@RequestHeader(WebhookHeaders.CHANNEL_ID) String channelId,
+		@RequestHeader(WebhookHeaders.CHANNEL_EXPIRATION) String channelExpiration,
+		@RequestHeader(WebhookHeaders.RESOURCE_STATE) String resourceState,
+		@RequestHeader(WebhookHeaders.MESSAGE_NUMBER) String messageNumber) {
+		googleService.listEvents(channelId);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 }
