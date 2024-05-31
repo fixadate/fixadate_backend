22d384eea7c4381dc4046f3458408417a3d29bcc
yongjunhong <kevin0928@naver.com>
2024-06-01 00:34:39 +0900

    FIX-27 :art: : 상황에 맞게 로그 설정

--- src/main/java/com/fixadate/global/util/LogAspect.java
+++ src/main/java/com/fixadate/global/util/LogAspect.java
@@ -1,74 +1,92 @@
 package com.fixadate.global.util;
 
-import lombok.extern.slf4j.Slf4j;
+import java.lang.reflect.Method;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
 
 @Aspect
 @Slf4j
-@Profile("local")
 @Component
 public class LogAspect {
 
-    @Pointcut("execution(* com.fixadate..*(..))")
-    public void all() {
-    }
+	@Pointcut("execution(* com.fixadate..*(..))")
+	public void all() {
+	}
 
-    @Pointcut("execution(* com.fixadate..*Controller.*(..))")
-    public void controller() {
-    }
+	@Pointcut("!execution(* com.fixadate..util.*.*(..))")
+	public void exceptUtil() {
+	}
 
-    @Pointcut("execution(* com.fixadate..*Service.*(..))")
-    public void service() {
-    }
+	@Pointcut("execution(* com.fixadate..*Controller.*(..))")
+	public void controller() {
+	}
 
-    @Around("all()")
-    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
-        try {
-            Object result = joinPoint.proceed();
-            return result;
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            log.info("log = {}", joinPoint.getSignature());
-            log.info("timeMs = {}", timeMs);
-        }
-    }
+	@Pointcut("execution(* com.fixadate..*Service.*(..))")
+	public void service() {
+	}
 
-    @Before("controller() || service()")
-    public void beforeLogic(JoinPoint joinPoint) throws Throwable {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        log.info("method = {}", method.getName());
+	@Profile("prod")
+	@Around("all() && exceptUtil()")
+	public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		try {
+			return joinPoint.proceed();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			log.info("log = {}", joinPoint.getSignature());
+			log.info("timeMs = {}", timeMs);
+		}
+	}
 
-        Object[] args = joinPoint.getArgs();
-        for (Object arg : args) {
-            if (arg != null) {
-                log.info("type = {}", arg.getClass().getSimpleName());
-                log.info("value = {}", arg);
-            }
-        }
-    }
+	@Before("(controller() || service()) && exceptUtil()")
+	public void beforeLogic(JoinPoint joinPoint) throws Throwable {
+		MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+		Method method = methodSignature.getMethod();
+		log.info("method = {}", method.getName());
 
-    @After("controller() || service()")
-    public void afterLogic(JoinPoint joinPoint) throws Throwable {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        log.info("method = {}", method.getName());
-        Object[] args = joinPoint.getArgs();
+		Object[] args = joinPoint.getArgs();
+		for (Object arg : args) {
+			if (arg != null) {
+				log.info("type = {}", arg.getClass().getSimpleName());
+				log.info("value = {}", arg);
+			}
+		}
+	}
 
-        for (Object arg : args) {
-            if (arg != null) {
-                log.info("type = {}", arg.getClass().getSimpleName());
-                log.info("value = {}", arg);
-            }
-        }
-    }
+	@After("(controller() || service()) && exceptUtil()")
+	public void afterLogic(JoinPoint joinPoint) throws Throwable {
+		MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+		Method method = methodSignature.getMethod();
+		log.info("method = {}", method.getName());
+		Object[] args = joinPoint.getArgs();
+
+		for (Object arg : args) {
+			if (arg != null) {
+				log.info("type = {}", arg.getClass().getSimpleName());
+				log.info("value = {}", arg);
+			}
+		}
+	}
+
+	@Profile({"test", "dev"})
+	@AfterThrowing(value = "all() && exceptUtil()", throwing = "e")
+	public void afterThrowingLogic(JoinPoint joinPoint, Throwable e) {
+		MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+		Method method = methodSignature.getMethod();
+		log.info("method = {}", method.getName());
+		e.printStackTrace();
+	}
 }
