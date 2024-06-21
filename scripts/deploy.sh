90781829b23bf8d3490dc2e01758e3edde8ca282
sungjun4403 <geulligu89@naver.com>
2024-06-22 02:09:27 +0900

    👷 : Blue/Green deployment script test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,25 +7,25 @@ APP_NAME=fixadate
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-#CURRENT_PID=$(pgrep -f $APP_NAME)
-CURRENT_PID=$(sudo netstat -ntlp | grep :8081 | awk '{print $7}' | cut -d'/' -f1)
-#CURRENT_PID=$(sudo netstat -ntlp | grep :8082 | awk '{print $7}' | cut -d'/' -f1)
+P8081_PID=$(sudo netstat -ntlp | grep :8081 | awk '{print $7}' | cut -d'/' -f1)
+P8082_PID=$(sudo netstat -ntlp | grep :8082 | awk '{print $7}' | cut -d'/' -f1)
+
 
 NEW_PID=0
 
-if [ -z "$CURRENT_PID" ]
+if [ -z "$P8081_PID" ] && [ -z "$P8082_PID" ]
 then
   NEW_PID=8081
   echo "> Deploy - $JAR_PATH "
   nohup java -jar $JAR_PATH --server.port=$NEW_PID > /home/ubuntu/deploy.log 2>&1 &
 
-elif [ "$CURRENT_PID" -eq 8081 ]
+elif [ -z "$P8082_PID" ]
 then
   NEW_PID=8082
   echo "> Deploy - $JAR_PATH "
   nohup java -jar $JAR_PATH --server.port=$NEW_PID > /home/ubuntu/deploy.log 2>&1 &
 
-elif [ "$CURRENT_PID" -eq 8082 ]
+elif [ -z "$P8081_PID" ]
 then
   NEW_PID=8081
   echo "> Deploy - $JAR_PATH "
