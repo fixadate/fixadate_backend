98b630b70df6690afcaf9944bdcefa0a74f45850
sungjun4403 <geulligu89@naver.com>
2024-06-22 00:55:42 +0900

    👷 : Blue/Green deployment script test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,13 @@ CURRENT_PID=$(sudo pgrep -f $APP_NAME)
 
 NEW_PID=0
 
-if [ ${CURRENT_PID} -eq 8081 ]
+if [ -z ${CURRENT_PID} ]
+then
+  NEW_PID=8081
+  echo "> Deploy - $JAR_PATH "
+  nohup java -jar $JAR_PATH --server.port=$NEW_PID > /home/ubuntu/deploy.log 2>&1 &
+  
+elif [ ${CURRENT_PID} -eq 8081 ]
 then
   NEW_PID=8082
   echo "> Deploy - $JAR_PATH "
