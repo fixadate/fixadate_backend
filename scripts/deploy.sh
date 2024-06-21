7e9779358d1ebbb3d8a3f7edd036c35289292582
sungjun4403 <geulligu89@naver.com>
2024-06-22 01:07:13 +0900

    👷 : Blue/Green deployment script test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,23 +7,23 @@ APP_NAME=fixadate
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(sudo pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 NEW_PID=0
 
-if [ -z ${CURRENT_PID} ]
+if [ -z $CURRENT_PID ]
 then
   NEW_PID=8081
   echo "> Deploy - $JAR_PATH "
   nohup java -jar $JAR_PATH --server.port=$NEW_PID > /home/ubuntu/deploy.log 2>&1 &
-  
-elif [ ${CURRENT_PID} -eq 8081 ]
+
+elif [ $CURRENT_PID -eq 8081 ]
 then
   NEW_PID=8082
   echo "> Deploy - $JAR_PATH "
   nohup java -jar $JAR_PATH --server.port=$NEW_PID > /home/ubuntu/deploy.log 2>&1 &
 
-elif [ ${CURRENT_PID} -eq 8082 ]
+elif [ $CURRENT_PID -eq 8082 ]
 then
   NEW_PID=8081
   echo "> Deploy - $JAR_PATH "
