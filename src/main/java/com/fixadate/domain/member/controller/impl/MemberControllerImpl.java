6671cafafe4023db80a4907dea8f799809b43a9e
yongjunhong <kevin0928@naver.com>
2024-08-05 16:31:41 +0900

    ️️♻️ MemberController, Service 리팩토링
    - 클래스 시작 부분 개행
    - 괄호 개행
    - Controller에서 adjs, nouns 필드 삭제
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>

--- src/main/java/com/fixadate/domain/member/controller/impl/MemberControllerImpl.java
+++ src/main/java/com/fixadate/domain/member/controller/impl/MemberControllerImpl.java
@@ -1,8 +1,5 @@
 package com.fixadate.domain.member.controller.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,23 +17,13 @@ import lombok.RequiredArgsConstructor;
 @RestControllerWithMapping("/v1/member")
 @RequiredArgsConstructor
 public class MemberControllerImpl implements MemberController {
-	private final MemberService memberService;
 
-	@Value("${randNick.adjs}")
-	private String randomAdjs;
-	@Value("${randNick.nouns}")
-	private String randomNouns;
+	private final MemberService memberService;
 
 	@Override
 	@GetMapping("/nickname")
 	public ResponseEntity<String> getRandomNickname() {
-		List<String> adjs = List.of(randomAdjs.split(","));
-		List<String> nouns = List.of(randomNouns.split(","));
-
-		String adj = memberService.getRandomNickname(adjs);
-		String noun = memberService.getRandomNickname(nouns);
-		String randomNickname = adj + " " + noun;
-		return ResponseEntity.ok(randomNickname);
+		return ResponseEntity.ok(memberService.generateRandomNickname());
 	}
 
 	@Override
@@ -47,8 +34,10 @@ public class MemberControllerImpl implements MemberController {
 
 	@Override
 	@PatchMapping("/{memberId}")
-	public ResponseEntity<MemberInfoResponse> updateMemberNickname(@PathVariable String memberId,
-		@RequestBody MemberInfoUpdateRequest memberInfoUpdateRequest) {
+	public ResponseEntity<MemberInfoResponse> updateMemberNickname(
+		@PathVariable String memberId,
+		@RequestBody MemberInfoUpdateRequest memberInfoUpdateRequest
+	) {
 		return ResponseEntity.ok(memberService.updateMemberInfo(memberId, memberInfoUpdateRequest));
 	}
 }
