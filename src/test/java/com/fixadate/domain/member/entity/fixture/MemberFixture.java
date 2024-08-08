2f2fe85dc90c8de48b5ac5d8c139b9148b07512b
yongjunhong <kevin0928@naver.com>
2024-08-08 19:38:10 +0900

    :white_check_mark: Member 클래스 테스트 구현
    Signed-off-by: yongjunhong <kevin0928@naver.com>

---
+++ src/test/java/com/fixadate/domain/member/entity/fixture/MemberFixture.java
@@ -0,0 +1,19 @@
+package com.fixadate.domain.member.entity.fixture;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import com.fixadate.domain.member.entity.Member;
+import com.fixadate.integration.config.FixtureMonkeyConfig;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+
+public class MemberFixture {
+
+	protected Member member;
+	protected FixtureMonkey fixtureMonkey = FixtureMonkeyConfig.jakartaValidationMonkey();
+
+	@BeforeEach
+	void setUp() {
+		member = fixtureMonkey.giveMeOne(Member.class);
+	}
+}
