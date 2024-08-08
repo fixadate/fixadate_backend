5b3a1de615369c9abe6840bd1dd830edf4c08c64
Yongjun Hong <kevin0928@naver.com>
2024-08-08 12:58:41 +0900

    🔀 TestContainer 싱글톤 패턴 적용 (#194)
    * :sparkles: TestContainer 생성
    
    - singleton 방식을 이용해 각 테스트마다 docker를 실행시키지 않고, 한 번만 실행시키도록 구현
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>
    
    * :art: 파일의 마지막 줄에 빈 줄 추가
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>
    
    * :art: 추상 클래스로 변경
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>
    
    * ♻️ RedisContainerProvider에서 redis 관련 설정을 하도록 수정
    
    - host, port, password 설정
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>
    
    * :see_no_evil: test yaml 공개
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>
    
    * :recycle: 클래스 수식자 final 로 수정
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>
    
    * :recycle: 마지막 줄에 개행 추가
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>
    
    ---------
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>

---
+++ src/test/java/com/fixadate/integration/config/MySqlContainerProvider.java
@@ -0,0 +1,18 @@
+package com.fixadate.integration.config;
+
+import org.testcontainers.containers.MySQLContainer;
+
+final class MySqlContainerProvider {
+
+	private static final MySQLContainer MY_SQL_CONTAINER;
+	private static final String IMAGE_VERSION = "mysql:8.0.31";
+
+
+	private MySqlContainerProvider() {
+	}
+
+	static {
+		MY_SQL_CONTAINER = new MySQLContainer(IMAGE_VERSION);
+		MY_SQL_CONTAINER.start();
+	}
+}
---
+++ src/test/java/com/fixadate/integration/config/RedisContainerProvider.java
@@ -0,0 +1,32 @@
+package com.fixadate.integration.config;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import com.redis.testcontainers.RedisContainer;
+
+final class RedisContainerProvider {
+
+	private static final String IMAGE_VERSION = "redis:5.0.7-alpine";
+	private static final String REDIS_PASSWORD = "REDIS_PASSWORD";
+	private static final Integer REDIS_PORT = 6379;
+	private static final RedisContainer REDIS_CONTAINER;
+
+	private RedisContainerProvider() {
+	}
+
+	static {
+		REDIS_CONTAINER = new RedisContainer(IMAGE_VERSION)
+			.withExposedPorts(REDIS_PORT)
+			.withCommand("--requirepass " + REDIS_PASSWORD)
+			.withReuse(true);
+		REDIS_CONTAINER.start();
+	}
+
+	@DynamicPropertySource
+	static void registerRedisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+		registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(REDIS_PORT).toString());
+		registry.add("spring.data.redis.password", () -> REDIS_PASSWORD);
+	}
+}
