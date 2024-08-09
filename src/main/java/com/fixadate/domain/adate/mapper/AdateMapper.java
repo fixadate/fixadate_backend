9c2f9f373bda3a0ab074cb2a6cbadc82593267ac
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-08-09 15:20:43 +0900

    :recycle: AdateMapper 코드 컨벤션에 따라 리팩터링

--- src/main/java/com/fixadate/domain/adate/mapper/AdateMapper.java
+++ src/main/java/com/fixadate/domain/adate/mapper/AdateMapper.java
@@ -1,7 +1,10 @@
 package com.fixadate.domain.adate.mapper;
 
-import static com.fixadate.domain.adate.entity.Adate.*;
-import static com.fixadate.domain.tag.mapper.TagMapper.*;
+import static com.fixadate.domain.adate.entity.Adate.builder;
+import static com.fixadate.domain.adate.entity.Adate.checkEndDateTimeIsNull;
+import static com.fixadate.domain.adate.entity.Adate.checkEventIsAllDayType;
+import static com.fixadate.domain.adate.entity.Adate.checkStartDateTimeIsNull;
+import static com.fixadate.domain.tag.mapper.TagMapper.toResponse;
 
 import com.fixadate.domain.adate.dto.request.AdateRegisterRequest;
 import com.fixadate.domain.adate.dto.response.AdateResponse;
@@ -13,45 +16,39 @@ import com.fixadate.domain.tag.entity.Tag;
 import com.fixadate.global.util.RandomValueUtil;
 import com.google.api.services.calendar.model.Event;
 
-/**
- *
- * @author yongjunhong
- * @since 2024. 6. 1.
- */
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AdateMapper {
 
-	private AdateMapper() {
+	public static Adate registerDtoToEntity(final AdateRegisterRequest adateRegisterRequest, final Member member) {
+		return builder().title(adateRegisterRequest.title())
+						.notes(adateRegisterRequest.notes())
+						.location(adateRegisterRequest.location())
+						.alertWhen(adateRegisterRequest.alertWhen())
+						.repeatFreq(adateRegisterRequest.repeatFreq())
+						.ifAllDay(adateRegisterRequest.ifAllDay())
+						.startsWhen(adateRegisterRequest.startsWhen())
+						.endsWhen(adateRegisterRequest.endsWhen())
+						.calendarId(RandomValueUtil.createRandomString(10))
+						.reminders(adateRegisterRequest.reminders())
+						.member(member)
+						.build();
 	}
 
-	public static Adate registerDtoToEntity(AdateRegisterRequest adateRegisterRequest, Member member) {
-		return builder()
-			.title(adateRegisterRequest.title())
-			.notes(adateRegisterRequest.notes())
-			.location(adateRegisterRequest.location())
-			.alertWhen(adateRegisterRequest.alertWhen())
-			.repeatFreq(adateRegisterRequest.repeatFreq())
-			.ifAllDay(adateRegisterRequest.ifAllDay())
-			.startsWhen(adateRegisterRequest.startsWhen())
-			.endsWhen(adateRegisterRequest.endsWhen())
-			.calendarId(RandomValueUtil.createRandomString(10))
-			.reminders(adateRegisterRequest.reminders())
-			.member(member)
-			.build();
-	}
-
-	public static Adate eventToEntity(Event event) {
-		return builder()
-			.title(event.getSummary())
-			.notes(event.getDescription())
-			.location(event.getLocation())
-			.color(event.getColorId())
-			.startsWhen(checkStartDateTimeIsNull(event))
-			.endsWhen(checkEndDateTimeIsNull(event))
-			.ifAllDay(checkEventIsAllDayType(event))
-			.calendarId(event.getId())
-			.etag(event.getEtag())
-			.reminders(event.getReminders().getUseDefault())
-			.build();
+	public static Adate eventToEntity(final Event event) {
+		return builder().title(event.getSummary())
+						.notes(event.getDescription())
+						.location(event.getLocation())
+						.color(event.getColorId())
+						.startsWhen(checkStartDateTimeIsNull(event))
+						.endsWhen(checkEndDateTimeIsNull(event))
+						.ifAllDay(checkEventIsAllDayType(event))
+						.calendarId(event.getId())
+						.etag(event.getEtag())
+						.reminders(event.getReminders().getUseDefault())
+						.build();
 	}
 
 	public static AdateResponse toAdateResponse(Adate adate) {
@@ -70,7 +67,7 @@ public class AdateMapper {
 		);
 	}
 
-	public static AdateViewResponse toAdateViewResponse(Adate adate) {
+	public static AdateViewResponse toAdateViewResponse(final Adate adate) {
 		return new AdateViewResponse(
 			adate.getTitle(),
 			adate.getNotes(),
@@ -82,10 +79,11 @@ public class AdateMapper {
 		);
 	}
 
-	private static TagResponse getTagResponse(Tag tag) {
+	private static TagResponse getTagResponse(final Tag tag) {
 		if (tag == null) {
 			return null;
 		}
+
 		return toResponse(tag);
 	}
 }
