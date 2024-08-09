fbfe71b95de07c132421e08be9ad545fad02feb1
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-08-09 18:26:26 +0900

    :bug: 테스트 실패 문제 해결
    일정의 태그가 null이 되지 않도록 함

--- src/test/java/com/fixadate/domain/adate/entity/fixture/AdateTestFixture.java
+++ src/test/java/com/fixadate/domain/adate/entity/fixture/AdateTestFixture.java
@@ -11,16 +11,21 @@ import com.navercorp.fixturemonkey.FixtureMonkey;
 @SuppressWarnings("NonAsciiCharacters")
 public class AdateTestFixture {
 
-	protected FixtureMonkey fixtureMonkey = FixtureMonkeyConfig.jakartaValidationMonkey();
+	protected final FixtureMonkey fixtureMonkey = FixtureMonkeyConfig.jakartaValidationMonkey();
 	protected Adate 일정;
 	protected Adate 수정_일정;
 	protected Tag 수정_태그;
 
 	@BeforeEach
 	void setUpFixture() {
+		final Tag 태그 = fixtureMonkey.giveMeBuilder(Tag.class)
+									.setNotNull("color")
+									.sample();
+
 		일정 = fixtureMonkey.giveMeBuilder(Adate.class)
 						  .set("ifAllDay", true)
 						  .set("reminders", true)
+						  .set("tag", 태그)
 						  .sample();
 
 		수정_일정 = fixtureMonkey.giveMeBuilder(Adate.class)
