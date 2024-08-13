dc96472d828ee3165ae13b0b7ad4837036ab6b1e
yongjunhong <kevin0928@naver.com>
2024-08-13 10:04:51 +0900

    :bug: RedisContainer 버그 수정
    Signed-off-by: yongjunhong <kevin0928@naver.com>

--- src/test/java/com/fixadate/config/RedisContainerProvider.java
+++ src/test/java/com/fixadate/config/RedisContainerProvider.java
@@ -1,32 +1,33 @@
 package com.fixadate.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-
-import com.redis.testcontainers.RedisContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
 final class RedisContainerProvider {
 
-	private static final String IMAGE_VERSION = "redis:5.0.7-alpine";
-	private static final String REDIS_PASSWORD = "REDIS_PASSWORD";
+	private static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("redis:7.0");
 	private static final Integer REDIS_PORT = 6379;
-	private static final RedisContainer REDIS_CONTAINER;
-
-	private RedisContainerProvider() {
-	}
+	private static final GenericContainer<?> REDIS_CONTAINER;
 
 	static {
-		REDIS_CONTAINER = new RedisContainer(IMAGE_VERSION)
-			.withExposedPorts(REDIS_PORT)
-			.withCommand("--requirepass " + REDIS_PASSWORD)
-			.withReuse(true);
+		REDIS_CONTAINER = new GenericContainer<>(DOCKER_IMAGE_NAME)
+			.withExposedPorts(REDIS_PORT);
 		REDIS_CONTAINER.start();
 	}
 
 	@DynamicPropertySource
 	static void registerRedisProperties(DynamicPropertyRegistry registry) {
 		registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
-		registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(REDIS_PORT).toString());
-		registry.add("spring.data.redis.password", () -> REDIS_PASSWORD);
+		registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(REDIS_PORT));
+	}
+
+	@Test
+	void givenRedisContainerConfiguredWithDynamicProperties_whenCheckingRunningStatus_thenStatusIsRunning() {
+		assertThat(REDIS_CONTAINER.isRunning()).isTrue();
 	}
 }
