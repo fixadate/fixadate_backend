d2fbc6a1110e7a312d6de45e382fe1839b765dec
yongjunhong <kevin0928@naver.com>
2024-08-13 19:25:07 +0900

    :bug: RedisContainerProvider 수정
    - @Configuration 어노테이션을 추가 함
    
    Signed-off-by: yongjunhong <kevin0928@naver.com>

--- src/test/java/com/fixadate/config/RedisContainerProvider.java
+++ src/test/java/com/fixadate/config/RedisContainerProvider.java
@@ -1,33 +1,24 @@
 package com.fixadate.config;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
-final class RedisContainerProvider {
+@Configuration
+@SuppressWarnings("NonAsciiCharacters")
+public class RedisContainerProvider {
 
-	private static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("redis:7.0");
+	private static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("redis:7.0.15-alpine");
 	private static final Integer REDIS_PORT = 6379;
 	private static final GenericContainer<?> REDIS_CONTAINER;
 
 	static {
 		REDIS_CONTAINER = new GenericContainer<>(DOCKER_IMAGE_NAME)
 			.withExposedPorts(REDIS_PORT);
+
 		REDIS_CONTAINER.start();
-	}
 
-	@DynamicPropertySource
-	static void registerRedisProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
-		registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(REDIS_PORT));
-	}
-
-	@Test
-	void givenRedisContainerConfiguredWithDynamicProperties_whenCheckingRunningStatus_thenStatusIsRunning() {
-		assertThat(REDIS_CONTAINER.isRunning()).isTrue();
+		System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+		System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(REDIS_PORT).toString());
 	}
 }
