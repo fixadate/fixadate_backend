69e5b8b337a076a5785005ab41ab4b8fafbf462f
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-08-21 00:55:04 +0900

    :sparkles: method arguments 예외와 그 외 예외 처리 추가

--- src/main/java/com/fixadate/global/exception/ControllerAdvice.java
+++ src/main/java/com/fixadate/global/exception/ControllerAdvice.java
@@ -1,10 +1,14 @@
 package com.fixadate.global.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fixadate.global.exception.badrequest.BadRequestException;
 import com.fixadate.global.exception.forbidden.ForbiddenException;
@@ -12,7 +16,7 @@ import com.fixadate.global.exception.notfound.NotFoundException;
 import com.fixadate.global.exception.unauthorized.UnAuthorizedException;
 
 @RestControllerAdvice
-public class ControllerAdvice {
+public class ControllerAdvice extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler({
 		NotFoundException.class
@@ -46,11 +50,28 @@ public class ControllerAdvice {
 							 .body(new ExceptionResponse(exception.getCode(), exception.getMessage()));
 	}
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(
-		MethodArgumentNotValidException exception
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		final MethodArgumentNotValidException exception,
+		final HttpHeaders headers,
+		final HttpStatusCode status,
+		final WebRequest request
 	) {
+		final String message = exception.getFieldErrors().get(0).getDefaultMessage();
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-							 .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage()));
+							 .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), message));
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(
+		final Exception exception,
+		final Object body,
+		final HttpHeaders headers,
+		final HttpStatusCode statusCode,
+		final WebRequest request
+	) {
+		return ResponseEntity.status(statusCode)
+							 .body(new ExceptionResponse(statusCode.value(), exception.getMessage()));
 	}
 }
