42287db48a26b57ed69a38d6c4d8cc034864f090
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-09-05 10:26:18 +0900

    :memo: 해결 TODO 삭제

--- src/main/java/com/fixadate/domain/adate/dto/response/AdateResponse.java
+++ src/main/java/com/fixadate/domain/adate/dto/response/AdateResponse.java
@@ -2,7 +2,6 @@ package com.fixadate.domain.adate.dto.response;
 
 import java.time.LocalDateTime;
 
-// TODO: [질문] AdateDto까지는 Boolean으로 null이 가능하기에 npe 가능성이 있습니다. 괜찮을까요?
 public record AdateResponse(
 	String title,
 	String notes,
