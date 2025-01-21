9d06a7a0261208efa49ff98fd64f13826a2f1a5e
DanielYY95 <asrs02041@naver.com>
2025-01-21 23:04:08 +0900

    [Test](팀 생성) 팀 생성 test code 기본 구조

---
+++ src/test/java/com/fixadate/integration/domain/dates/service/TeamServiceTest.java
@@ -0,0 +1,109 @@
+package com.fixadate.integration.domain.dates.service;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fixadate.config.DataClearExtension;
+import com.fixadate.domain.dates.dto.TeamCreateRequest;
+import com.fixadate.domain.dates.entity.Teams;
+import com.fixadate.domain.dates.repository.TeamRepository;
+import com.fixadate.domain.dates.service.TeamService;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ExtendWith(DataClearExtension.class)
+@SpringBootTest
+@Testcontainers
+class TeamServiceTest {
+
+    @Autowired
+    private TeamService teamService;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    private static final String MESSAGE = "message";
+    
+    @Container
+    static MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8.0.31");
+
+    @BeforeAll
+    static void initDataBase(@Autowired DataSource dataSource) {
+        try (Connection conn = dataSource.getConnection()) {
+            mySQLContainer.start();
+            ScriptUtils.executeSqlScript(conn, new ClassPathResource("/sql/init/dropTable.sql"));
+            ScriptUtils.executeSqlScript(conn, new ClassPathResource("/sql/init/dates_test.sql"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterAll
+    static void stopContainers() {
+        mySQLContainer.stop();
+    }
+
+    @Nested
+    @DisplayName("Team 저장 테스트")
+    class CreateTeamTest {
+        
+        @DisplayName("정상적으로 팀이 저장되는 경우")
+        @ParameterizedTest(name = "{index}번째 팀 생성")
+        @CsvSource(value = {
+            "DevTeam, 개발팀입니다",
+            "DesignTeam, 디자인팀입니다",
+            "MarketingTeam, 마케팅팀입니다",
+            "SalesTeam, 영업팀입니다",
+            "HRTeam, 인사팀입니다"
+        })
+        void createTeamTestSuccess(
+            @AggregateWith(TeamAggregator.class) TeamCreateRequest teamCreateRequest
+        ) {
+            Assertions.assertDoesNotThrow(() -> teamService.createTeam(teamCreateRequest));
+
+            Optional<Teams> teamOptional = teamRepository.findByName(teamCreateRequest.name());
+            
+            assertTrue(teamOptional.isPresent());
+            Teams team = teamOptional.get();
+
+            assertAll(
+                () -> assertEquals(teamCreateRequest.name(), team.getName()),
+                () -> assertEquals(teamCreateRequest.description(), team.getDescription())
+            );
+        }
+    }
+
+    static class TeamAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws
+            ArgumentsAggregationException {
+            return new TeamCreateRequest(
+                argumentsAccessor.getString(0),
+                argumentsAccessor.getString(1)
+            );
+        }
+    }
+}
