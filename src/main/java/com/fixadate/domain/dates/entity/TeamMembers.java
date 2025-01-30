e9e1e0027f0e567867df00b1a76e30b74ec8aa6e
sungjun4403 <geulligu89@naver.com>
2025-01-30 22:42:31 +0900

    [Debug 🐛] (conflit 해소)

--- src/main/java/com/fixadate/domain/dates/entity/TeamMembers.java
+++ src/main/java/com/fixadate/domain/dates/entity/TeamMembers.java
@@ -2,25 +2,11 @@ package com.fixadate.domain.dates.entity;
 
 import com.fixadate.domain.auth.entity.BaseTimeEntity;
 import com.fixadate.domain.member.entity.Member;
-import com.fixadate.domain.member.entity.Permissions;
-import com.fixadate.domain.member.entity.Plans;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import org.codehaus.jackson.annotate.JsonBackReference;
-<<<<<<< Updated upstream
-=======
-import org.joda.time.DateTime;
-import org.joda.time.LocalDateTime;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
->>>>>>> Stashed changes
+
 
 @Getter
 @Entity
@@ -39,16 +25,15 @@ public class TeamMembers extends BaseTimeEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-<<<<<<< Updated upstream
+
     @Column
     private String updatedBy;
-=======
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Grades grades;
 
-    @Column
-    private String updatedBy;
+
 
     @Builder
     private TeamMembers(
@@ -63,5 +48,7 @@ public class TeamMembers extends BaseTimeEntity {
         this.updatedBy = updatedBy;
     }
 
->>>>>>> Stashed changes
+    public TeamMembers() {
+
+    }
 }
