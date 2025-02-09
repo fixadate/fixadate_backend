be84b2f4a68fa3e0bf09e33f7ecf649867385b6f
DanielYY95 <asrs02041@naver.com>
2025-02-09 19:53:53 +0900

    [Feat](공통) Plan별 resource 갯수 관리

---
+++ src/main/java/com/fixadate/domain/member/repository/MemberResourcesRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.fixadate.domain.member.repository;
+
+import com.fixadate.domain.member.entity.Member;
+import com.fixadate.domain.member.entity.MemberResources;
+import com.fixadate.domain.member.entity.ResourceType;
+import com.fixadate.domain.member.service.repository.MemberResourcesRepository;
+import com.fixadate.global.exception.notfound.NotFoundException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberResourcesRepositoryImpl implements MemberResourcesRepository {
+    private final MemberResourcesJpaRepository memberResourcesJpaRepository;
+
+    @Override
+    public MemberResources getMemberResources(Member member) {
+        Optional<MemberResources> memberResources = memberResourcesJpaRepository.findByMember(member);
+        return memberResources.orElseThrow(
+            () -> new NotFoundException(null)
+        );
+    }
+
+    @Override
+    public int getResourceCnt(Member member, ResourceType resourceType) {
+        Optional<MemberResources> memberResources = memberResourcesJpaRepository.findByMember(member);
+        if(memberResources.isEmpty()){
+            throw new NotFoundException(null);
+        }
+        return memberResources.get().getResourceCnt(resourceType);
+    }
+
+    @Override
+    public boolean addMemberResources(Member member, ResourceType resourceType, int cnt) {
+        MemberResources memberResources = getMemberResources(member);
+        int resourceCnt = memberResources.getResourceCnt(resourceType);
+        memberResources.plusResources(resourceType, cnt);
+        return resourceCnt == memberResources.getResourceCnt(resourceType);
+    }
+
+    @Override
+    public boolean removeMemberResources(Member member, ResourceType resourceType, int cnt) {
+        MemberResources memberResources = getMemberResources(member);
+        int resourceCnt = memberResources.getResourceCnt(resourceType);
+        memberResources.minusResources(resourceType, cnt);
+        return resourceCnt == memberResources.getResourceCnt(resourceType);
+    }
+}
