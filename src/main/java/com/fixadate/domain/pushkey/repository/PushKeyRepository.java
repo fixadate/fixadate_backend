7aad7a57440f480731b981534fbd6668fa4afd79
DanielYY95 <asrs02041@naver.com>
2025-02-15 20:23:43 +0900

    [Feat](notification) firebase 메시지 service

--- src/main/java/com/fixadate/domain/pushkey/repository/PushKeyRepository.java
+++ src/main/java/com/fixadate/domain/pushkey/repository/PushKeyRepository.java
@@ -1,12 +1,23 @@
 package com.fixadate.domain.pushkey.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fixadate.domain.pushkey.entity.PushKey;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PushKeyRepository extends JpaRepository<PushKey, Long> {
 
 	public Optional<PushKey> findPushKeyByPushKey(String key);
+
+	boolean existsByMemberId(String memberId);
+
+	Optional<PushKey> findByMemberId(String memberId);
+
+	void deleteByMemberId(String memberId);
+
+	List<PushKey> findAllByPushKey(String pushKey);
 }
