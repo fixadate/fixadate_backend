3fef2ea626f9ec659404268ffb64515638dc2bef
DanielYY95 <asrs02041@naver.com>
2025-02-16 20:35:35 +0900

    [Refactor](Notification) Page response swagger 명시

--- src/main/java/com/fixadate/domain/notification/controller/NotificationController.java
+++ src/main/java/com/fixadate/domain/notification/controller/NotificationController.java
@@ -1,10 +1,18 @@
 package com.fixadate.domain.notification.controller;
 
+import com.fixadate.domain.notification.dto.NotificationPageResponse;
 import com.fixadate.domain.notification.service.NotificationService;
 import com.fixadate.global.dto.GeneralResponseDto;
 import com.fixadate.global.factory.PageFactory;
 import com.fixadate.global.jwt.MemberPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -20,15 +28,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Alarm", description = "푸시 알림 API")
+@Tag(name = "NotificationController", description = "푸시 알림 API")
 @RequestMapping("/alarm")
 public class NotificationController {
     private final NotificationService notificationService;
 
-    @Tag(name = "Alarm")
     @Operation(summary = "내 푸시 알림 목록 조회", description = "")
+    @Parameters(value = {
+        @Parameter(name = "page", description = "페이지 번호입니다.",
+            content = @Content(schema = @Schema(implementation = String.class)), in = ParameterIn.QUERY),
+        @Parameter(name = "size", description = "페이지 사이즈입니다.",
+            content = @Content(schema = @Schema(implementation = String.class)), in = ParameterIn.QUERY)
+    })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "푸시 알림 paging response",
+            content = @Content(schema = @Schema(implementation = NotificationPageResponse.class))),
+        @ApiResponse(responseCode = "401", description = "jwt 만료되었을 때 생기는 예외",
+            content = @Content(schema = @Schema(implementation = Void.class))),
+        @ApiResponse(responseCode = "404", description = "securityContext에 있는 값으로 member를 찾을 수 없는 경우",
+            content = @Content(schema = @Schema(implementation = Void.class)))
+    })
     @GetMapping("/list")
-    public GeneralResponseDto getAlarmList(
+    public GeneralResponseDto getNotificationList(
         @AuthenticationPrincipal final MemberPrincipal memberPrincipal,
         @RequestParam("page") int page,
         @RequestParam("size") int size
@@ -37,21 +58,43 @@ public class NotificationController {
         return GeneralResponseDto.create("200", "read success", notificationService.getNotificationList(memberPrincipal.getMember(), newPageable));
     }
 
-    @Tag(name = "Alarm")
     @Operation(summary = "푸시 알림 읽음", description = "")
+    @Parameters(value = {
+        @Parameter(name = "id", description = "push 고유번호",
+            content = @Content(schema = @Schema(implementation = Long.class)), in = ParameterIn.QUERY)
+    })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "성공인 경우, true",
+            content = @Content(schema = @Schema(implementation = Boolean.class))),
+        @ApiResponse(responseCode = "401", description = "jwt 만료되었을 때 생기는 예외",
+            content = @Content(schema = @Schema(implementation = Void.class))),
+        @ApiResponse(responseCode = "404", description = "securityContext에 있는 값으로 member를 찾을 수 없는 경우",
+            content = @Content(schema = @Schema(implementation = Void.class)))
+    })
     @PatchMapping("/{id}/status")
-    public GeneralResponseDto changeAlarmRead(
+    public GeneralResponseDto changeNotificationRead(
         @AuthenticationPrincipal final MemberPrincipal memberPrincipal,
         @PathVariable("id") Long id) {
-        return GeneralResponseDto.create("200", "read success", notificationService.changeAlarmRead(id, memberPrincipal.getMember()));
+        return GeneralResponseDto.create("200", "read success", notificationService.changeNotificationRead(id, memberPrincipal.getMember()));
     }
 
-    @Tag(name = "Alarm")
     @Operation(summary = "푸시 알림 삭제", description = "")
+    @Parameters(value = {
+        @Parameter(name = "id", description = "push 고유번호",
+            content = @Content(schema = @Schema(implementation = Long.class)), in = ParameterIn.QUERY)
+    })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "성공인 경우, true",
+            content = @Content(schema = @Schema(implementation = Boolean.class))),
+        @ApiResponse(responseCode = "401", description = "jwt 만료되었을 때 생기는 예외",
+            content = @Content(schema = @Schema(implementation = Void.class))),
+        @ApiResponse(responseCode = "404", description = "securityContext에 있는 값으로 member를 찾을 수 없는 경우",
+            content = @Content(schema = @Schema(implementation = Void.class)))
+    })
     @DeleteMapping("/{id}/status")
-    public GeneralResponseDto deleteAlarm(
+    public GeneralResponseDto deleteNotification(
         @AuthenticationPrincipal final MemberPrincipal memberPrincipal,
         @PathVariable("id") Long id) {
-        return GeneralResponseDto.create("200", "delete success", notificationService.deleteAlarm(id, memberPrincipal.getMember()));
+        return GeneralResponseDto.create("200", "delete success", notificationService.deleteNotification(id, memberPrincipal.getMember()));
     }
 }
