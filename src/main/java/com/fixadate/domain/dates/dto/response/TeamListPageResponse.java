dbd94910c4d4172de42a070e1e71bc3cf0fdbb80
DanielYY95 <asrs02041@naver.com>
2025-03-22 21:46:59 +0900

    [Bugfix](공통) class명이 같을 때, swagger에서 구분 못하는 문제

--- src/main/java/com/fixadate/domain/dates/dto/response/TeamListPageResponse.java
+++ src/main/java/com/fixadate/domain/dates/dto/response/TeamListPageResponse.java
@@ -1,12 +1,13 @@
 package com.fixadate.domain.dates.dto.response;
 
+import com.fixadate.domain.dates.dto.response.TeamListResponse.TeamListEach;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import org.springframework.data.domain.Page;
 
 @Schema(name = "TeamListPageResponse", description = "Page response for teams!!!")
 public record TeamListPageResponse(
-    List<TeamListResponse.Each> content,
+    List<TeamListEach> content,
     int pageNumber,
     int pageSize,
     long totalElements,
@@ -14,7 +15,7 @@ public record TeamListPageResponse(
     boolean last
 ) {
 
-    public TeamListPageResponse(Page<TeamListResponse.Each> page) {
+    public TeamListPageResponse(Page<TeamListEach> page) {
         this(page.getContent(), page.getNumber(), page.getSize(), page.getTotalElements(), page.getTotalPages(), page.isLast());
     }
 
