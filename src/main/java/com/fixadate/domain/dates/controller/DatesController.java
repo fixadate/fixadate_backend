ab2308a0436b44dfc10c2567c0f58b0d7043c87e
DanielYY95 <asrs02041@naver.com>
2025-03-26 12:48:38 +0900

    [Refactor](Dates) Dates메인

--- src/main/java/com/fixadate/domain/dates/controller/DatesController.java
+++ src/main/java/com/fixadate/domain/dates/controller/DatesController.java
@@ -1,10 +1,12 @@
 package com.fixadate.domain.dates.controller;
 
 
+import com.fixadate.domain.adate.dto.response.AdateInfoResponse;
 import com.fixadate.domain.dates.dto.*;
 import com.fixadate.domain.dates.dto.request.DatesRegisterRequest;
 import com.fixadate.domain.dates.dto.request.DatesUpdateRequest;
 import com.fixadate.domain.dates.dto.response.DatesDetailResponse;
+import com.fixadate.domain.dates.dto.response.DatesInfoResponse;
 import com.fixadate.domain.dates.dto.response.DatesResponse;
 import com.fixadate.domain.dates.mapper.DatesMapper;
 import com.fixadate.domain.dates.service.DatesService;
@@ -12,13 +14,24 @@ import com.fixadate.domain.main.dto.DatesMemberInfo;
 import com.fixadate.domain.member.entity.Member;
 import com.fixadate.global.dto.GeneralResponseDto;
 import com.fixadate.global.jwt.MemberPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "DatesController", description = "DatesController 입니다.")
 @RestController
 @RequestMapping("/v1/teams")
 public class DatesController {
@@ -72,6 +85,45 @@ public class DatesController {
         final Member member = memberPrincipal.getMember();
         DatesDetailResponse result = datesService.getDatesDetail(id, member);
         return GeneralResponseDto.success("", result);
+    }
+
+    @Operation(summary = "Dates 메인 조회(주별)", description = "Dates 메인 조회를 등록합니다.")
+    @Parameter(name = "accessToken", description = "Authorization : Bearer + <jwt>", in = ParameterIn.HEADER)
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "success",
+            content = @Content(schema = @Schema(implementation = Void.class))),
+        @ApiResponse(responseCode = "401", description = "jwt 만료되었을 때 생기는 예외",
+            content = @Content(schema = @Schema(implementation = Void.class))),
+    })
+    @GetMapping("/week")
+    public GeneralResponseDto getDatesBy(
+        @AuthenticationPrincipal final MemberPrincipal memberPrincipal,
+        @RequestParam final int year,
+        @RequestParam @Min(1) @Max(12) final int month,
+        @RequestParam @Min(1) @Max(6) final int weekNum
+    ) {
+        final Member member = memberPrincipal.getMember();
+        final DatesInfoResponse response = datesService.getDatesByWeek(member, year, month, weekNum);
+        return GeneralResponseDto.success("", "");
+    }
+
+    @Operation(summary = "Dates 메인 조회(월별)", description = "Dates 메인 조회를 등록합니다.")
+    @Parameter(name = "accessToken", description = "Authorization : Bearer + <jwt>", in = ParameterIn.HEADER)
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "success",
+            content = @Content(schema = @Schema(implementation = Void.class))),
+        @ApiResponse(responseCode = "401", description = "jwt 만료되었을 때 생기는 예외",
+            content = @Content(schema = @Schema(implementation = Void.class))),
+    })
+    @GetMapping("/month")
+    public GeneralResponseDto getDatesBy(
+        @AuthenticationPrincipal final MemberPrincipal memberPrincipal,
+        @RequestParam final int year,
+        @RequestParam @Min(1) @Max(12) final int month
+    ) {
+        final Member member = memberPrincipal.getMember();
+        final DatesInfoResponse response = datesService.getDatesByMonth(member, year, month);
+        return GeneralResponseDto.success("", "");
     }
 }
 
--- src/main/java/com/fixadate/domain/dates/service/DatesService.java
+++ src/main/java/com/fixadate/domain/dates/service/DatesService.java
@@ -1,27 +1,47 @@
 package com.fixadate.domain.dates.service;
 
+import static com.fixadate.global.exception.ExceptionCode.INVALID_START_END_TIME;
+import static com.fixadate.global.util.TimeUtil.getLocalDateTimeFromYearAndMonth;
+
+import com.fixadate.domain.adate.dto.response.AdateInfoResponse;
+import com.fixadate.domain.adate.dto.response.AdateInfoResponse.DailyAdateInfo;
+import com.fixadate.domain.adate.dto.response.AdateResponse;
+import com.fixadate.domain.adate.entity.Adate;
+import com.fixadate.domain.adate.entity.ToDo;
 import com.fixadate.domain.auth.entity.BaseEntity.DataStatus;
 import com.fixadate.domain.dates.dto.DatesDto;
 import com.fixadate.domain.dates.dto.DatesRegisterDto;
 import com.fixadate.domain.dates.dto.DatesUpdateDto;
 import com.fixadate.domain.dates.dto.response.DatesDetailResponse;
+import com.fixadate.domain.dates.dto.response.DatesInfoResponse;
+import com.fixadate.domain.dates.dto.response.DatesInfoResponse.DailyDatesInfo;
+import com.fixadate.domain.dates.dto.response.DatesResponse;
 import com.fixadate.domain.dates.entity.Dates;
 import com.fixadate.domain.dates.entity.Grades;
 import com.fixadate.domain.dates.entity.TeamMembers;
 import com.fixadate.domain.dates.entity.Teams;
 import com.fixadate.domain.dates.mapper.DatesMapper;
+import com.fixadate.domain.dates.repository.DatesQueryRepository;
 import com.fixadate.domain.dates.repository.DatesRepository;
 import com.fixadate.domain.dates.repository.TeamMembersRepository;
 import com.fixadate.domain.dates.repository.TeamRepository;
+import com.fixadate.domain.main.dto.TodoInfo;
 import com.fixadate.domain.member.entity.Member;
 import com.fixadate.domain.notification.event.object.DatesCreateEvent;
 import com.fixadate.domain.notification.event.object.DatesDeleteEvent;
+import com.fixadate.global.exception.badrequest.InvalidTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -32,6 +52,9 @@ public class DatesService {
     private final TeamMembersRepository teamMembersRepository;
     private final DatesRepository datesRepository;
     private final ApplicationEventPublisher applicationEventPublisher;
+    private final DatesQueryRepository datesQueryRepository;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
 
     public DatesDto createDates(DatesRegisterDto requestDto, Member member) {
         final Teams foundTeam = teamRepository.findById(requestDto.teamId()).orElseThrow(
@@ -154,4 +177,77 @@ public class DatesService {
         return null;
     }
 
+    public DatesInfoResponse getDatesByWeek(Member member, int year, int month, int weekNum) {
+        // 주의 첫째 날(일요일) 구하기
+        LocalDate firstDay = LocalDate.of(year, month, 1)
+            .with(WeekFields.ISO.weekOfWeekBasedYear(), weekNum)
+            .with(WeekFields.ISO.dayOfWeek(), 7); // 일요일은 7
+
+        // 주의 마지막 날(토요일) 구하기
+        LocalDate lastDay = firstDay.plusDays(6);
+
+        // LocalDateTime으로 변환
+        LocalDateTime firstDateTime = firstDay.atStartOfDay();
+        LocalDateTime lastDateTime = lastDay.atStartOfDay();
+
+        return getDatesByStartAndEndTime(member, firstDateTime, lastDateTime);
+    }
+
+    public DatesInfoResponse getDatesByMonth(Member member, int year, int month) {
+        final LocalDateTime startTime = getLocalDateTimeFromYearAndMonth(year, month, true);
+        final LocalDateTime endTime = getLocalDateTimeFromYearAndMonth(year, month, false);
+
+        return getDatesByStartAndEndTime(member, startTime, endTime);
+    }
+
+    private void checkStartAndEndTime(final LocalDateTime startTime, final LocalDateTime endTime) {
+        if (startTime.isAfter(endTime)) {
+            throw new InvalidTimeException(INVALID_START_END_TIME);
+        }
+    }
+
+    // todo: 날짜별로 Adate 세팅하도록 변경 필요. 너무 많은 데이터 조회
+    public DatesInfoResponse getDatesByStartAndEndTime(
+        final Member member,
+        final LocalDateTime startDateTime,
+        final LocalDateTime endDateTime
+    ) {
+        checkStartAndEndTime(startDateTime, endDateTime);
+        final List<Dates> datesList = findByDateRange(member, startDateTime, endDateTime);
+        List<DatesResponse> datesInfos = datesList.stream().map(dates -> DatesResponse.of(dates, new ArrayList<>())).toList();
+
+        DatesInfoResponse dateInfos = new DatesInfoResponse();
+        dateInfos.setDateInfos(startDateTime, endDateTime);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        for(DailyDatesInfo dateInfo : dateInfos.getDateList()) {
+            List<DatesResponse> datesInfosByDate = new ArrayList<>();
+            for(DatesResponse datesInfo : datesInfos) {
+                if(datesInfo.startDate().equals(dateInfo.getDate())) {
+                    datesInfosByDate.add(datesInfo);
+                }
+            }
+            // 이미 진행된 건은 제외
+            if(dateInfo.isToday()){
+                List<DatesResponse> todayAdateInfos = new ArrayList<>(datesInfosByDate);
+                todayAdateInfos.removeIf(datesResponse -> LocalDateTime.parse(datesResponse.endsWhen(), formatter).isBefore(now));
+                dateInfo.setDatesResponseList(todayAdateInfos);
+            }else {
+                dateInfo.setDatesResponseList(datesInfosByDate);
+            }
+
+            dateInfo.setTotalCnt(); // todo: 오늘 날짜로 이미 지난 일정도 포함해야하는가?
+        }
+
+        return dateInfos;
+    }
+
+    List<Dates> findByDateRange(
+        final Member member,
+        final LocalDateTime startDateTime,
+        final LocalDateTime endDateTime
+    ){
+        return datesQueryRepository.findByDateRange(member, startDateTime, endDateTime);
+    };
 }
