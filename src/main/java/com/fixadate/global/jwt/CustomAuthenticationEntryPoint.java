c44c8989325c0484859b624cce9e8f9b04297d30
sungjun4403 <geulligu89@naver.com>
2025-04-24 01:18:57 +0900

    [Refactor 🎨](logging jwt error): set logging for jwt CustomAuthenticationEntryPoint

--- src/main/java/com/fixadate/global/jwt/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/fixadate/global/jwt/CustomAuthenticationEntryPoint.java
@@ -2,6 +2,7 @@ package com.fixadate.global.jwt;
 
 import java.io.IOException;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,10 +12,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
+@Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException authException) throws IOException, ServletException {
+		log.error("401 | " + authException);
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 	}
 }
