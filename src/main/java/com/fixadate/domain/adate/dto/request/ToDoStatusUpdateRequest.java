fd9d2c3094c37c4c7ed5751296f5f316e70c3933
sungjun4403 <geulligu89@naver.com>
2025-06-01 01:16:25 +0900

    [Debug 🐛](Cannot use notblank on non-string): modified ToDoStatusUpdateRequest)

--- src/main/java/com/fixadate/domain/adate/dto/request/ToDoStatusUpdateRequest.java
+++ src/main/java/com/fixadate/domain/adate/dto/request/ToDoStatusUpdateRequest.java
@@ -1,11 +1,12 @@
 package com.fixadate.domain.adate.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ToDoStatusUpdateRequest {
-        @NotBlank
+        @NotNull
         Long id;
         @NotBlank
         String toDoStatus;
